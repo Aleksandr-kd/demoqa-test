5b10196f89ef12a9d169b5c3c03c06b1c8ab8829
ASArutyunyan <aleks_pochta_krd@mail.ru>
2024-09-04 23:11:18 +0300

    added project start

---
+++ src/test/java/demoqa/TextBoxTest.java
@@ -0,0 +1,23 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+    }
+}
