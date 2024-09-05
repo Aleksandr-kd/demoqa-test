9fd080524340b8f672c27da4bf55fb79c8722d80
ASArutyunyan <aleks_pochta_krd@mail.ru>
2024-09-05 14:28:37 +0300

    added TextBoxTest

--- src/test/java/demoqa/TextBoxTest.java
+++ src/test/java/demoqa/TextBoxTest.java
@@ -3,21 +3,41 @@ package demoqa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTest {
 
     @BeforeAll
     static void beforeAll() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER); // Устанавливаем стратегию EAGER
+
+        Configuration.browserCapabilities = options;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     void fillFormTest() {
+        String userName = "Alex Arut";
+        String userEmail = "abc@mail.ru";
+
         open("/text-box");
-        $(".main-header").shouldHave(text("Text Box"));
+        $(".text-center").shouldHave(text("Text Box"));
+
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue("Krd city");
+        $("#permanentAddress").setValue("Krd city 2");
+        $("#submit").click();
+        $("#output").$("#name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        //todo: проверить, что поле ввода текстового поля существует
+        sleep(4000);
     }
 }
