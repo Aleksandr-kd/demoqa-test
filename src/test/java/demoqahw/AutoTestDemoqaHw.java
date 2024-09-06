b92cd89e6e18491632e0d6952e137c6d62fedf17
ASArutyunyan <aleks_pochta_krd@mail.ru>
2024-09-06 17:57:56 +0300

    public class AutoTestDemoqaHw WithComments

--- src/test/java/demoqahw/AutoTestDemoqaHw.java
+++ src/test/java/demoqahw/AutoTestDemoqaHw.java
@@ -4,10 +4,10 @@ package demoqahw;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -18,12 +18,13 @@ public class AutoTestDemoqaHw {
     @BeforeAll
     static void beforeAll() {
         ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER); // Устанавливаем стратегию EAGER
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
         Configuration.browserCapabilities = options;
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-}
+        Configuration.browserSize = "1920x1080";
+    }
+
 
 
     @Test
@@ -31,36 +32,34 @@ public class AutoTestDemoqaHw {
         String firstName = "Alex";
         String lastName = "Arut";
         String userEmail = "alex@gmail.com";
+        String userNumber = "1234567890";
 
         open("/automation-practice-form");
         $(".text-center").shouldHave(text("Practice Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $$(".custom-radio").get(1).click();
-        $("#userNumber").setValue("1234567890");
-//        $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "a");
-//        $("#dateOfBirthInput").sendKeys(Keys.DELETE);
-//        $("#dateOfBirthInput").setValue("19 Dec 1996");
-//        $(".subjects-auto-complete__placeholder css-1wa3eu0-placeholder").click();
-        $("#subjectsInput").setValue("m").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click();
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1986");
+        $("#userNumber").setValue(userNumber);
+        $(".react-datepicker__day--030:not(.react-datepicker__day--day--outside-month)").click();
+        $("#subjectsInput").setValue("maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue("Moscow");
         $("#state").click();
         $(byText("NCR")).click();
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
-        sleep(4000);
 
-        $("#closeLargeModal").click();
-
-
-        // col-md-9 col-sm-12  subjects-auto-complete__value-container subjects-auto-complete__placeholder css-1wa3eu0-placeholder
-
-        sleep(8000);
-
-
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(userEmail), text(userNumber));
     }
 }
